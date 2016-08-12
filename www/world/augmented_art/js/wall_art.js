dda9c20831a39b72f102af44d37ad6259e942d03
John Shen <john.shen@minted.com>
2016-08-11 17:37:08 -0700

    add better markers

--- www/world/augmented_art/js/wall_art.js
+++ www/world/augmented_art/js/wall_art.js
@@ -2,29 +2,19 @@ var World = {
   loaded: false,
   pages: [
     {
-      key: 1,
-      name: "page1",
-      // we don't use the markerPath in code, it is embedded in markers.wtc
-      // the markerPath is included here for reference
-      markerPath: "assets/001.png",
+      name: "001",
       overlayPath: "assets/products/MIN-4A2-GNA_24x18.jpg"
     },
     {
-      key: 2,
-      name: "page2",
-      markerPath: "assets/002.png",
+      name: "002",
       overlayPath: "assets/products/MIN-88G-GNA_24x24.jpg"
     },
     {
-      key: 3,
-      name: "page3",
-      markerPath: "assets/003.png",
+      name: "003",
       overlayPath: "assets/products/MIN-F3B-GNA_24x18.jpg"
     },
     {
-      key: 4,
-      name: "page4",
-      markerPath: "assets/004.png",
+      name: "004",
       overlayPath: "assets/products/MIN-HCK-GNA_24X18.jpg"
     },
   ],
@@ -36,7 +26,7 @@ var World = {
 
   createTracker: function() {
     // init tracker
-    this.tracker = new AR.ClientTracker("assets/markers.wtc", {
+    this.tracker = new AR.ClientTracker("assets/markers2.wtc", {
       onLoaded: this.worldLoaded
     });
   },
